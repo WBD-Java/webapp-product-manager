521853292b971f3f35605e8f5ee1a6e9dc66287d
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-19 16:19:19 +0700

    update function display product list

--- src/main/java/com/codegym/controller/ProductServlet.java
+++ src/main/java/com/codegym/controller/ProductServlet.java
@@ -1,16 +1,19 @@
 package com.codegym.controller;
 
+import com.codegym.model.Product;
 import com.codegym.service.ProductService;
 import com.codegym.service.ProductServiceImpl;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "ProductServlet", urlPatterns = "/customers")
+@WebServlet(name = "ProductServlet", urlPatterns = "/products")
 public class ProductServlet extends HttpServlet {
     private ProductService productService = new ProductServiceImpl();
 
@@ -28,6 +31,26 @@ public class ProductServlet extends HttpServlet {
         String action = request.getParameter("action");
         if (action == null) {
             action = "";
+        }
+
+        switch (action) {
+            default:
+                list(request, response);
+                break;
+        }
+    }
+
+    private void list(HttpServletRequest request, HttpServletResponse response) {
+        List<Product> products = this.productService.findAll();
+        request.setAttribute("products", products);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("product/list.jsp");
+        try {
+            dispatcher.forward(request,response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/codegym/service/ProductServiceImpl.java
+++ src/main/java/com/codegym/service/ProductServiceImpl.java
@@ -2,7 +2,9 @@ package com.codegym.service;
 
 import com.codegym.model.Product;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProductServiceImpl implements ProductService {
@@ -14,5 +16,10 @@ public class ProductServiceImpl implements ProductService {
         products.put(3, new Product(3, "Hộp Cắm Bút HV08", 99000, "Làm bằng gỗ tự nhiên", "Nhatvywood"));
         products.put(4, new Product(4, "Bút Xoá FO-CP01", 19000, "Mực bút xóa ra đều", "Flexoffice"));
         products.put(5, new Product(5, "Tẩy Stabilo Exam", 16000, "Tẩy chì hiệu quả, rất được ưa chuộng, đóng gói độc lập, chống bụi.", "Stabilo"));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
     }
 }
