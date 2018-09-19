7c5b9a6dff32c59925d8dad0cc0bf5d0ed6c6782
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-19 13:44:18 +0700

    create ProductServlet to direction action

---
+++ src/main/java/com/codegym/controller/ProductServlet.java
@@ -0,0 +1,33 @@
+package com.codegym.controller;
+
+import com.codegym.service.ProductService;
+import com.codegym.service.ProductServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ProductServlet", urlPatterns = "/customers")
+public class ProductServlet extends HttpServlet {
+    private ProductService productService = new ProductServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            action = "";
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+    }
+}
