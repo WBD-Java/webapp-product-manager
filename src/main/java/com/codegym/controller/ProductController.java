8e7f727d0e2d57a651d4e54bb6c83593c747d5c4
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-21 17:29:26 +0700

    update function create Product

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -4,9 +4,9 @@ import com.codegym.model.Product;
 import com.codegym.service.ProductService;
 import com.codegym.service.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
 @Controller
@@ -14,9 +14,22 @@ public class ProductController {
     private ProductService productService = new ProductServiceImpl();
 
     @GetMapping(value = "/products")
-    public String showListProducts(ModelMap model) {
+    public String showListProducts(Model model) {
         List<Product> products = this.productService.findAll();
         model.addAttribute("products", products);
         return "/product/list";
+    }
+
+    @GetMapping(value = "/product/create")
+    public String showFormCreate(Model model) {
+        model.addAttribute("product", new Product());
+        return "/product/create";
+    }
+
+    @PostMapping(value = "/product/save")
+    public String createProduct(Product product) {
+        product.setId((int) (Math.random()*1000));
+        productService.save(product);
+        return "redirect:/products";
     }
 }
