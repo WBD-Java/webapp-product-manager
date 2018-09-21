b82d25671511c83f5769478d4b31eac0244580d5
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-21 15:52:05 +0700

    update function showListProduct

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -1,10 +1,22 @@
 package com.codegym.controller;
 
+import com.codegym.model.Product;
 import com.codegym.service.ProductService;
 import com.codegym.service.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
     private ProductService productService = new ProductServiceImpl();
+
+    @GetMapping(value = "/products")
+    public String showListProducts(ModelMap model) {
+        List<Product> products = this.productService.findAll();
+        model.addAttribute("products", products);
+        return "/product/list";
+    }
 }
