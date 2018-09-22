be7ba4e512328cde2e19dce4622af041d070c454
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-09-22 09:28:42 +0700

    update function edit product

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -6,7 +6,11 @@ import com.codegym.service.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 
 @Controller
@@ -31,5 +35,27 @@ public class ProductController {
         product.setId((int) (Math.random()*1000));
         productService.save(product);
         return "redirect:/products";
+    }
+
+    @GetMapping("product/edit")
+    public ModelAndView showEditForm(@RequestParam("id") int id) {
+        Product product = this.productService.findById(id);
+        ModelAndView modelAndView;
+        if (product == null) {
+            modelAndView = new ModelAndView("error.404");
+            return modelAndView;
+        }
+        modelAndView = new ModelAndView("/product/edit");
+        modelAndView.addObject("product", product);
+        return modelAndView;
+    }
+
+    @PostMapping("product/update/{id}")
+    public ModelAndView updateProduct(@PathVariable("id")int id, Product product) {
+        productService.update(id, product);
+        ModelAndView modelAndView = new ModelAndView("/product/edit");
+        modelAndView.addObject("product", product);
+        modelAndView.addObject("messages", "Product updated successfully");
+        return modelAndView;
     }
 }
